a6d4a3a450e9455ebcccb32b6dcd1648b42b108e
Chen Zhao <chen10220@gmail.com>
2018-09-26 12:11:37 -0700

    initial commit: react, router, petfinder api, prettier, parcel setup

---
+++ src/Carousel.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+class Carousel extends React.Component {
+  state = {
+    photos: [],
+    active: 0
+  };
+
+  static getDerivedStateFromProps({ media }) {
+    let photos = [];
+
+    if (media && media.photos && media.photos.photo) {
+      photos = media.photos.photo.filter(photo => photo["@size"] === "pn");
+    }
+
+    return { photos };
+  }
+
+  handleIndexClick = event => {
+    this.setState({
+      active: +event.target.dataset.index //coerce with + (string to numbr)
+    });
+  };
+
+  render() {
+    const { photos, active } = this.state;
+    return (
+      <div className="carousel">
+        <img src={photos[active].value} alt="primary animal" />
+        <div className="carousel-smaller">
+          {photos.map((photo, index) => (
+            /* eslint-disable-next-line */
+            <img
+              key={photo.value}
+              data-index={index}
+              src={photo.value}
+              alt="animal thumbnail"
+              className={index === active ? "active" : ""}
+              onClick={this.handleIndexClick}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
